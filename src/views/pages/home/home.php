8fdf1a78a85a37e8ebee93fc9cc6ad286adc6970
epitacio araujo <epitacio.sobrinho00@hotmail.com>
2025-06-04 08:54:15 -0300

    Remove unused images and styles, update home page layout and title

--- src/views/pages/home/home.php
+++ src/views/pages/home/home.php
@@ -1,1 +1,14 @@
-olá, lock box
+<div class="hero min-h-screen flex max-w-screen-lg mx-auto">
+    <div class="hero-content flex-col lg:flex-row-reverse">
+        <div>
+            <p>Bem vindo ao</p>
+            <h1 class="text-5xl font-bold">Lock box</h1>
+            <p class="py-6">
+                Onde você guarda <strong>tudo</strong> com segurança
+            </p>
+
+            <button class="btn btn-link">Sign in</button>
+            <button class="btn btn-link">Sign up</button>
+        </div>
+    </div>
+</div>
--- src/views/partials/layout/layout.php
+++ src/views/partials/layout/layout.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-BR" data-theme="dracula">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 
-    <?php include realpath(__DIR__ . "/../../../public/assets/styles/styles.php") ?>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
 
-    <title>Bookwise</title>
+    <title>Lock box</title>
 </head>
 
 <body>
